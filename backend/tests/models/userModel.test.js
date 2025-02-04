c0d6d449651fc39e228a110c0d72b4447117087a
freddy <89398269+kamenifg01@users.noreply.github.com>
2025-02-04 02:24:27 +0100

    Delete backend directory

--- backend/tests/models/userModel.test.js
+++
@@ -1,8 +0,0 @@
-const { User } = require('../models');
-
-describe('User Model', () => {
-  it('should create a user', async () => {
-    const user = await User.create({ email: 'test@example.com', password: 'password' });
-    expect(user.email).toBe('test@example.com');
-  });
-}); 
