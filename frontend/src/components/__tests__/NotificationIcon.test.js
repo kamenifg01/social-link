2e7caf84f4fc4557b50a7aa8b13c01723a7805b4
freddy <89398269+kamenifg01@users.noreply.github.com>
2025-02-04 02:24:57 +0100

    Delete frontend directory

--- frontend/src/components/__tests__/NotificationIcon.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import NotificationIcon from '../NotificationIcon';
-
-test('renders notification icon with badge', () => {
-  const { getByText } = render(<NotificationIcon count={5} />);
-
-  expect(getByText('5')).toBeTruthy();
-}); 
--- frontend/src/types/theme.js
+++
@@ -1,4 +0,0 @@
-export const THEME_TYPES = {
-  LIGHT: 'light',
-  DARK: 'dark'
-}; 
